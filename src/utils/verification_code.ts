f67b1e394f08ba88b4edb1bd663c608cc536fc11
Omar Omran <omaromran2003@gmail.com>
2025-03-01 15:53:33 +0200

    use otp instead of characters

--- src/utils/verification_code.ts
+++ src/utils/verification_code.ts
@@ -1,12 +1,7 @@
 import crypto from "crypto";
 
-export function generateVerificationCode(): string {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let verificationCode = "";
-  for (let i = 0; i < 6; i++) {
-    const randomIndex = crypto.randomInt(0, characters.length);
-    verificationCode += characters[randomIndex];
-  }
-
-  return verificationCode;
+function generateOTP(): string {
+  return crypto.randomInt(100000, 999999).toString();
 }
+
+export { generateOTP };
