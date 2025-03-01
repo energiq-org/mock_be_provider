9748bdf51fbd2e4a4479d142f2ff728df0265c30
Omar Omran <omaromran2003@gmail.com>
2025-03-01 15:52:21 +0200

    use logging from env

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -1,17 +1,19 @@
 import { Sequelize } from "sequelize";
 import config from "./env";
 
-export const sequelize = new Sequelize({
+const sequelize = new Sequelize({
   database: config.DB_NAME,
   username: config.DB_USERNAME,
   password: config.DB_PASSWORD,
   host: config.DB_HOST,
   port: config.DB_PORT,
   dialect: "postgres",
-  logging: true,
+  logging: config.DB_LOGGING,
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false,
     },
   },
 });
+
+export { sequelize };
