f7982397dce651daa191727aa92cfc4528fa74a4
Omar Omran <omaromran2003@gmail.com>
2025-03-02 23:34:25 +0200

    remove express types

--- src/types/express.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
-import { UUID } from "crypto";
-import { Response } from "express";
-
-interface AuthorizedResponse extends Response<unknown, { userId: UUID }> {}
-export { AuthorizedResponse };
