36a2dd969ffd377cc1958a31afeba3d5c7aa3cf4
Omar Omran <omaromran2003@gmail.com>
2025-03-09 01:20:51 +0200

    do not remove this!

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-
 import express from "express";
 // import * as OpenApiValidator from "express-openapi-validator";
 import { apiReference } from "@scalar/express-api-reference";
@@ -55,16 +54,16 @@ function createServer() {
   // server.use(OpenApiValidator.middleware(validatorOptions));
 
   // error customization, if request is invalid
-  server.use((err: object, _req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.status(Number(err["status"]) || 500).json({
-      error: {
-        type: "request_validation",
-        message: err["message"] as string,
-        errors: err["errors"] as object,
-      },
-    });
-    next();
-  });
+  // server.use((err: object, _req: express.Request, res: express.Response, next: express.NextFunction) => {
+  //   res.status(Number(err["status"]) || 500).json({
+  //     error: {
+  //       type: "request_validation",
+  //       message: err["message"] as string,
+  //       errors: err["errors"] as object,
+  //     },
+  //   });
+  //   next();
+  // });
 
   return server;
 }
