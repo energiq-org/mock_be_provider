996539c4ac47b9354732a9b5f93467f2a24bbb88
Omar Omran <omaromran2003@gmail.com>
2025-03-09 01:16:11 +0200

    add scalar and swagger ui

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
 import express from "express";
 // import * as OpenApiValidator from "express-openapi-validator";
+import { apiReference } from "@scalar/express-api-reference";
 import morgan from "morgan";
 import morganBody from "morgan-body";
-import { summarise } from "swagger-routes-express";
-import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 import config from "./config/env.ts";
+import { docs } from "./docs/index.ts";
 import { authRouter } from "./routers/auth.ts";
 import { usersRouter } from "./routers/users.ts";
 import { vehiclesRouter } from "./routers/vehicles.ts";
-import logger from "./utils/logging.ts";
 
 function createServer() {
-  const yamlSpecFile = "./openapi.yml";
-  const apiDefinition = YAML.load(yamlSpecFile) as object;
-  const apiSummary = summarise(apiDefinition);
-  logger.info(apiSummary);
   const server = express();
+
   server.use(express.json());
 
   if (config.HTTP_LOGGING) {
@@ -26,18 +27,32 @@ function createServer() {
     morganBody(server);
   }
 
-  // setup API validator
-  // const validatorOptions = {
-  //   apiSpec: yamlSpecFile,
-  //   validateRequests: config.SPEC_REQUEST_VALIDATION,
-  //   validateResponses: config.SPEC_RESPONSE_VALIDATION,
-  // };
-
-  // server.use(OpenApiValidator.middleware(validatorOptions));
-  
   server.use("/api/v1/auth", authRouter);
   server.use("/api/v1/vehicles", vehiclesRouter);
   server.use("/api/v1/users", usersRouter);
+
+  const openAPIDocs = docs.generateDocument(docs.document, server._router, docs.options.basePath);
+
+  server.use(docs);
+
+  server.use(
+    "/api/v1/docs/scalar",
+    apiReference({
+      spec: {
+        content: openAPIDocs,
+      },
+    })
+  );
+  server.use("/api/v1/docs/swagger", swaggerUi.serve, swaggerUi.setup(openAPIDocs));
+
+  // setup API validator
+  // const validatorOptions: OpenApiValidatorOpts = {
+  //   apiSpec: openAPIDocs,
+  //   validateResponses: true,
+  //   validateApiSpec: true,
+  // };
+
+  // server.use(OpenApiValidator.middleware(validatorOptions));
 
   // error customization, if request is invalid
   server.use((err: object, _req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -50,6 +65,7 @@ function createServer() {
     });
     next();
   });
+
   return server;
 }
 
