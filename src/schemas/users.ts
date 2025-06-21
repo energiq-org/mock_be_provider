1a1aade4746715770965f6063f5e3760177598ba
Omar Omran <omaromran2003@gmail.com>
2025-06-21 03:49:34 +0300

    just min length of 8

--- src/schemas/users.ts
+++ src/schemas/users.ts
@@ -12,9 +12,9 @@ const userSchema = Type.Object({
         description: "Email address",
     }),
     password: Type.String({
-        pattern: "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,}$",  
-        description:
-            "Password must be at least 8 characters long and contain at least one letter and one number and one special character",
+        minLength: 8,
+        // description:
+        //     "Password must be at least 8 characters long and contain at least one letter and one number and one special character",
     }),
     email_verified: Type.Boolean(),
     phone_number: Type.String({
